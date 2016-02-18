0ffa008fe07674ed68486f9e34396fcd831c77eb
Thomas Flori <tflori@pb-systems.de>
2016-02-18 08:48:37 +0100

    install, build and test made working

---
+++ bundles/angular2-translator.js
@@ -0,0 +1,5 @@
+System.registerDynamic("angular2-translator", [], false, function(__require, __exports, __module) {
+  var _retrieveGlobal = System.get("@@global-helpers").prepareGlobal(__module.id, null, null);
+  (function() {})();
+  return _retrieveGlobal();
+});
