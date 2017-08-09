09952aafabb0fc5104e9007310c0e19873c91f21
Thomas Flori <thflori@gmail.com>
2017-08-09 17:32:31 +0200

    implement a fake loader that always returns a predefined object

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export * from "./src/TranslateLogHandler";
 export * from "./src/TranslatePipe";
 export * from "./src/TranslationLoader";
 export * from "./src/TranslationLoader/Json";
+export * from "./src/TranslationLoader/Fake";
 export * from "./src/Translator";
 export * from "./src/TranslatorConfig";
 export * from "./src/TranslatorContainer";
--- src/TranslationLoader/Json.ts
+++ src/TranslationLoader/Json.ts
@@ -44,16 +44,4 @@ export class TranslationLoaderJson extends TranslationLoader {
                 );
         });
     }
-
-    private flattenTranslations(translations: any, data: any, prefix: string = "") {
-        for (let key in data) {
-            if (Array.isArray(data[key])) {
-                translations[prefix + key] = data[key].filter((v: any) => typeof v === "string").join("");
-            } else if (typeof data[key] === "object") {
-                this.flattenTranslations(translations, data[key], prefix + key + ".");
-            } else if (typeof data[key] === "string") {
-                translations[prefix + key] = data[key];
-            }
-        }
-    }
 }
